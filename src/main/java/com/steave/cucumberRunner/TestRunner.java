7bbc7ed00a49e6949d899feb121b133f2793802c
SteaveDsouza <steave.dsouza@outlook.com>
2021-07-19 20:47:48 -0400

    BDD extent Fix

--- src/main/java/com/steave/cucumberRunner/TestRunner.java
+++ src/main/java/com/steave/cucumberRunner/TestRunner.java
@@ -1,40 +1,37 @@
 package com.steave.cucumberRunner;
 
-import com.steave.constants.FrameWorkConstants;
 import com.steave.driver.Driver;
-import com.steave.utils.PropertyUtils;
-import com.vimalselvam.cucumber.listener.Reporter;
+import com.vimalselvam.cucumber.listener.ExtentCucumberFormatter;
 import cucumber.api.CucumberOptions;
 import cucumber.api.testng.CucumberFeatureWrapper;
 import cucumber.api.testng.TestNGCucumberRunner;
-import org.testng.annotations.*;
-
-import java.awt.*;
-import java.io.File;
-import java.io.IOException;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = {"com.steave.stepdefs"},
-        plugin = { "com.vimalselvam.cucumber.listener.ExtentCucumberFormatter:extent-test-output/index.html"},
+        plugin = { "com.steave.listener.ExtentCucumberFormatter:extent-test-output/index.html"},
         tags = {"@Sanity_Test"}
         )
 
 public class TestRunner {
     private TestNGCucumberRunner testNGCucumberRunner;
+    ExtentCucumberFormatter extent;
 
     @BeforeClass(alwaysRun = true)
     public void setUpClass(){
         testNGCucumberRunner = new TestNGCucumberRunner(this.getClass());
         Driver.initDriver( "chrome" );
-
     }
 
 
     @Test(groups = "cucumber", description = "Runs Cucumber Feature", dataProvider = "features")
     public void feature(CucumberFeatureWrapper cucumberFeature) {
         testNGCucumberRunner.runCucumber(cucumberFeature.getCucumberFeature());
-        Reporter.assignAuthor( "Chrome" );
+
     }
 
     @DataProvider
--- src/main/java/com/steave/cucumberRunner/TestRunnerFireFox.java
+++ src/main/java/com/steave/cucumberRunner/TestRunnerFireFox.java
@@ -1,9 +1,10 @@
 package com.steave.cucumberRunner;
 
+import com.aventstack.extentreports.ExtentTest;
 import com.steave.constants.FrameWorkConstants;
 import com.steave.driver.Driver;
+import com.steave.listener.Reporter;
 import com.steave.utils.PropertyUtils;
-import com.vimalselvam.cucumber.listener.Reporter;
 import cucumber.api.CucumberOptions;
 import cucumber.api.testng.CucumberFeatureWrapper;
 import cucumber.api.testng.TestNGCucumberRunner;
@@ -16,24 +17,26 @@ import java.io.IOException;
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = {"com.steave.stepdefs"},
-        plugin = { "com.vimalselvam.cucumber.listener.ExtentCucumberFormatter:extent-test-output/index.html"},
+        plugin = { "com.steave.listener.ExtentCucumberFormatter:extent-test-output/index.html"},
         tags = {"@Sanity_Test"}
         )
 
 public class TestRunnerFireFox{
     private TestNGCucumberRunner testNGCucumberRunner;
 
+    ExtentTest extent ;
+
     @BeforeClass(alwaysRun = true)
     public void setUpClass(){
         testNGCucumberRunner = new TestNGCucumberRunner(this.getClass());
         Driver.initDriver( "firefox" );
+
     }
 
 
     @Test(groups = "cucumber", description = "Runs Cucumber Feature", dataProvider = "features")
     public void feature(CucumberFeatureWrapper cucumberFeature) {
         testNGCucumberRunner.runCucumber(cucumberFeature.getCucumberFeature());
-        Reporter.assignAuthor( "firefox" );
     }
 
     @DataProvider
